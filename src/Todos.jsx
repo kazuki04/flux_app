c0d0d059808cac0213a2dec3e08cfc76a616b9f0
kazuki04 <kazukik0402@gmail.com>
2021-09-19 16:55:10 +0900

    delete App component

---
+++ src/Todos.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default class Todo extends React.Component {
+  constructor(){
+
+  }
+
+
+
+  return() {
+    <div>
+      <ul>
+        <li>test1</li>
+        <li>test2</li>
+        <li>test3</li>
+      </ul>
+    </div>
+  }
+}
